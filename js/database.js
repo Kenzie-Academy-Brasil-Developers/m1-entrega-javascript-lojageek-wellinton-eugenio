be0b4b7e83e6a56d3dfab36e70356399c23b6e95
wellington-eugenio <wellington.eliteinfo@gmail.com>
2022-09-02 16:43:43 -0300

    inclusao de Dados

---
+++ js/database.js
@@ -0,0 +1,41 @@
+const itensDaLoja = [
+    {
+        imagem: "animemoman.jpg",
+        name: "Evangelion figure",
+        price: 80.00,
+        type: "action"
+    },
+    {
+        imagem: "dragonbollpersonagem.jpg",
+        name: "Dragom boll Goky SSJ",
+        price: 120.00,
+        type: "action"
+    },
+    {
+        imagem: "starwarspersonagem.js",
+        name: "Baby Yoda action",
+        price: 95.00,
+        type: "action"
+    },
+    {
+        imagem: "clock.jpg",
+        name: "Quadro Clock",
+        price: 40.00,
+        type: "frame"
+    },
+    {
+        imagem: "gamepad.jpg",
+        name: "Quadro Gamer",
+        price: 99.00,
+        type: "frame"
+    },
+    {
+        imagem: "animemoman.jpg",
+        name: "Evangelion figure",
+        price: 80.00,
+        type: "frame"
+    },
+]
+
+let listFigures = [];
+let listFrames = [];
